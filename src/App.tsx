ef4cf7a10979d9ee0d9382990a4a9bf4b686080a
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-09-16 14:09:14 +0200

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./assets/scss/App.scss";
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <div id="App">
+      <Container className="py-3">
+        <h1>Tjosan!</h1>
+      </Container>
+
+    </div>
+  );
+}
+
+export default App;
+``;
