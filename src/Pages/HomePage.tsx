12a5430885af9cfd282c1c9f58bddb7987ee6e4e
Astrid Lindfors <astrid.lindfors@gmail.com>
2023-09-21 22:01:09 +0200

    renders out hardcoded markers

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,35 +1,90 @@
-import { useMemo } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  InfoWindow,
+  MarkerF,
+  useLoadScript,
+} from "@react-google-maps/api";
+import { useState } from "react";
 import "../assets/scss/App.scss";
 
-const HomePage = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_FIREBASE_GOOGLE_API_KEY,
-  });
+interface MarkerData {
+  address: string;
+  lat: number;
+  lng: number;
+}
 
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+interface InfoWindowData {
+  id: number;
+  address: string;
+}
+
+const HomePage: React.FC = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: String(import.meta.env.VITE_FIREBASE_GOOGLE_API_KEY),
+  });
+  const [mapRef, setMapRef] = useState<google.maps.Map | undefined>();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [infoWindowData, setInfoWindowData] = useState<
+    InfoWindowData | undefined
+  >();
+
+  const markers: MarkerData[] = [
+    { address: "Address1", lat: 18.5204, lng: 73.8567 },
+    { address: "Address2", lat: 18.5314, lng: 73.8446 },
+    { address: "Address3", lat: 18.5642, lng: 73.7769 },
+  ];
+
+  const onMapLoad = (map: google.maps.Map) => {
+    setMapRef(map);
+    const bounds = new google.maps.LatLngBounds();
+    markers.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+    map.fitBounds(bounds);
+  };
+
+  const handleMarkerClick = (
+    id: number,
+    lat: number,
+    lng: number,
+    address: string
+  ) => {
+    mapRef?.panTo({ lat, lng });
+    setInfoWindowData({ id, address });
+    setIsOpen(true);
+  };
 
   return (
-    <Container fluid className="Homepage">
-      <Row className="h-100">
-        <Col xs={12} className="map-container">
-          {!isLoaded ? (
-            <h1>Loading...</h1>
-          ) : (
-            <GoogleMap
-              mapContainerClassName="map-container"
-              center={center}
-              zoom={10}
+    <div className="Homepage">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          onLoad={onMapLoad}
+          onClick={() => setIsOpen(false)}
+        >
+          {markers.map(({ address, lat, lng }, ind) => (
+            <MarkerF
+              key={ind}
+              position={{ lat, lng }}
+              onClick={() => {
+                handleMarkerClick(ind, lat, lng, address);
+              }}
             >
-              <MarkerF position={{ lat: 18.52043, lng: 73.856743 }} />
-            </GoogleMap>
-          )}
-        </Col>
-      </Row>
-    </Container>
+              {isOpen && infoWindowData?.id === ind && (
+                <InfoWindow
+                  onCloseClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  <h3>{infoWindowData.address}</h3>
+                </InfoWindow>
+              )}
+            </MarkerF>
+          ))}
+        </GoogleMap>
+      )}
+    </div>
   );
 };
 
 export default HomePage;
-``;
